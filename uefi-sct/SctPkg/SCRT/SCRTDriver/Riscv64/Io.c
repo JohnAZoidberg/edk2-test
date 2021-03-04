ce36e0c448430c3182b8ffaeeac57f307b896edb
Daniel Schaefer <daniel.schaefer@hpe.com>
2021-03-04 11:06:33 +0800

    SctPkg: Remove trailing whitespace
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=3042
    
    Cc: G Edhaya Chandran <Edhaya.Chandran@arm.com>
    Cc: Barton Gao <gaojie@byosoft.com.cn>
    Cc: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>
    Cc: Eric Jin <eric.jin@intel.com>
    Cc: Arvin Chen <arvinx.chen@intel.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Cc: Abner Chang <abner.chang@hpe.com>
    Signed-off-by: Daniel Schaefer <daniel.schaefer@hpe.com>

--- uefi-sct/SctPkg/SCRT/SCRTDriver/Riscv64/Io.c
+++ uefi-sct/SctPkg/SCRT/SCRTDriver/Riscv64/Io.c
@@ -1,17 +1,17 @@
 /** @file
 
   Copyright 2006 - 2012 Unified EFI, Inc.<BR>
-  Copyright (c) 2011 - 2012 ARM Ltd. All rights reserved.<BR>   
+  Copyright (c) 2011 - 2012 ARM Ltd. All rights reserved.<BR>
   (C) Copyright 2021 Hewlett Packard Enterprise Development LP<BR>
 
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at 
+  which accompanies this distribution.  The full text of the license may be found a
   http://opensource.org/licenses/bsd-license.php
- 
+
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
- 
+
 **/
 
 /*++
--- uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/DebugSupport/BlackBoxTest/Aarch64/DebugSupportBBTestExceptionCallbackFunction.c
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/DebugSupport/BlackBoxTest/Aarch64/DebugSupportBBTestExceptionCallbackFunction.c
@@ -5,12 +5,12 @@
 
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at 
+  which accompanies this distribution.  The full text of the license may be found a
   http://opensource.org/licenses/bsd-license.php
- 
+
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
- 
+
 **/
 /*++
 
@@ -273,4 +273,3 @@ BBTestRegisterExceptionCallbackFunctionManualTest (
 
   return EFI_SUCCESS;
 }
-
